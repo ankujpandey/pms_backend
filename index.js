9f2126e6d8e7f5de4e08b9c0c0ffe85ff788d51f
ankujpandey <pandeyankuj@gmail.com>
2023-06-23 18:41:47 +0530

    .

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+console.log("<<<<<<<-----------Backend Started----------->>>>>>>>>");
+
+const app = express();
+app.use(cors());
+
+// to accept data in json formate and urlencoded formate
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set default indian local time
+process.env.TZ = process.env.TIME_ZONE;
+
+// to initiate routes
+// app.use("/api", ApiRoutes);
+
+app.listen(PORT, () => {
+	console.log(`Server Started At Port---------->>>>>>>${PORT}`);
+});
